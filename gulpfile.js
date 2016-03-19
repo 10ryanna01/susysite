fc8756603db5669949fd5e9f2d4606ad373b65b6
ryann al-lahiq <ryann.allahiq@gmail.com>
2016-03-19 17:05:47 +0000

    combined all js into one script call

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,54 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+  gp_concat = require('gulp-concat'),
+  gp_rename = require('gulp-rename'),
+  gp_uglify = require('gulp-uglify');
 
+
+gulp.task('compress', function() {
+    return gulp.src(['./js/jquery.js', './js/default.js', './js/superagent.js', './js/dom-event.js'])
+     .pipe(gp_concat('prettyjs.js'))
+     .pipe(gulp.dest('./web'))
+      .pipe(gp_rename('index.js'))
+     .pipe(gp_uglify())
+     .pipe(gulp.dest('./web'));
+    
+});
+
+
+
+var compass = require('gulp-compass');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+
+
+
+
+ 
+
+var modernizr = require('gulp-modernizr');
+gulp.task('modernizr', function() {
+  gulp.src('./js/*.js')
+    .pipe(modernizr())
+    .pipe(gulp.dest("./web"))
+});
+
+
+ 
+
+
+ 
+
+ 
+
+
+
+
+
+
+var sass = require('gulp-sass');
 // Gulp Sass Task 
 gulp.task('sass', function() {
-  gulp.src('./scss/{,*/}*.{scss,sass}')
+  gulp.src('./scss/styles.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
       errLogToConsole: true
@@ -21,5 +65,6 @@ gulp.task('sass', function() {
 // http://www.zell-weekeat.com/gulp-libsass-with-susy/#comment-1910185635
 gulp.task('default', ['sass'], function () {
   gulp.watch('./scss/{,*/}*.{scss,sass}', ['sass'])
+ 
 });
-
+  
